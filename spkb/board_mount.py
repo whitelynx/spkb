c76676c5329b953d3c313dd3126fb9c01f430d1c
David H. Bronke <whitelynx@gmail.com>
2024-07-02 16:04:15 +0200

    Added overlap to connector
    ...so we don't get the ghosted face between connector and plug.

--- spkb/board_mount.py
+++ spkb/board_mount.py
@@ -52,11 +52,11 @@ class BoardMount:
         """
         return optional(self.has_connector)(
             up(distance_from_surface - 1.25)(
-                forward(self.plug_offset)(
+                forward(self.plug_offset + 0.01)(
                     rotate((90, 0, 0))(
                         hull()(
-                            left(4 - 1.25)(cylinder_outer(2.5 / 2, 6)),
-                            right(4 - 1.25)(cylinder_outer(2.5 / 2, 6)),
+                            left(4 - 1.25)(cylinder_outer(2.5 / 2, 6.02)),
+                            right(4 - 1.25)(cylinder_outer(2.5 / 2, 6.02)),
                         )
                     )
                 )
