a905ee4101cf4d623960875050a6b33a1c19a070
David H. Bronke <whitelynx@gmail.com>
2024-11-05 22:45:51 +0100

    fix(spkb): include spkb.single_key_pcb.single_key_board in the root __all__

--- spkb/__init__.py
+++ spkb/__init__.py
@@ -1,6 +1,7 @@
 from .utils import cylinder_outer, nothing, optional
 from .board_mount import BoardMount
 from .key_grid_tester import key_grid_tester
+from .single_key_pcb import single_key_board
 from .single_tester import single_tester
 from .switch_plate import mx_plate, mx_backplate, mx_backplate_clearance, mx_plate_with_backplate, switch_plate
 
@@ -8,6 +9,7 @@ __all__ = [
     'cylinder_outer', 'nothing', 'optional',
     'BoardMount',
     'key_grid_tester',
+    'single_key_board',
     'single_tester',
     'mx_plate', 'mx_backplate', 'mx_backplate_clearance', 'mx_plate_with_backplate', 'switch_plate',
 ]
