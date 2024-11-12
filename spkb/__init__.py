0366b03032ca4451b08f278357479ca6a602f097
David H. Bronke <whitelynx@gmail.com>
2024-11-13 00:57:16 +0100

    feat(spkb): added keyswitch module

--- spkb/__init__.py
+++ spkb/__init__.py
@@ -1,6 +1,7 @@
 from .utils import cylinder_outer, nothing, optional
 from .board_mount import BoardMount
 from .key_grid_tester import key_grid_tester
+from .keyswitch import mx_keyswitch
 from .single_key_pcb import single_key_board
 from .single_tester import single_tester
 from .switch_plate import mx_plate, mx_backplate, mx_backplate_clearance, mx_plate_with_backplate, switch_plate
@@ -9,6 +10,7 @@ __all__ = [
     'cylinder_outer', 'nothing', 'optional',
     'BoardMount',
     'key_grid_tester',
+    'mx_keyswitch',
     'single_key_board',
     'single_tester',
     'mx_plate', 'mx_backplate', 'mx_backplate_clearance', 'mx_plate_with_backplate', 'switch_plate',
---
+++ spkb/keyswitch.py
@@ -0,0 +1,31 @@
+from solid2 import cube, cylinder, hull, mirror, rotate, up, down, left, right, forward, back, scad_render_to_file
+
+from .switch_plate import (
+    keyswitch_depth,
+    plate_thickness,
+)
+from .utils import cylinder_outer
+
+
+mx_midline_width = 13.95
+mx_midline_length = 15.6
+mx_topline_width = 10.5
+mx_topline_length = 10.5
+mx_height_above_plate = 6.2
+
+
+def mx_keyswitch():
+    """Build an simplified approximation of (the top half of) an MX-style keyswitch.
+    """
+    return hull()(
+        cube(mx_midline_width, mx_midline_length, 0.1, center=True).up(3.05),
+        cube(mx_topline_width, mx_topline_length, 0.1, center=True).up(mx_height_above_plate + 2.95),
+    )
+
+
+# To test, use the command line: pipenv run python -m spkb.single_key_pcb
+if __name__ == "__main__":
+    print("Rendering single_key_board() to single_key_board.scad...")
+    scad_render_to_file(
+        single_key_board(), filename="single_key_board.scad", file_header=f"$fn = {SEGMENTS};"
+    )
