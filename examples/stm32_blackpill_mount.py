6aa0a87c1c1f89e2ee2d99487f01be51bc1925e0
David H. Bronke <whitelynx@gmail.com>
2024-12-11 22:10:37 +0100

    fix(examples/stm32_blackpill_mount.py): fixed example so it runs

--- examples/stm32_blackpill_mount.py
+++ examples/stm32_blackpill_mount.py
@@ -3,8 +3,8 @@ from spkb.board_mount import stm32_blackpill
 if __name__ == "__main__":
     (
         stm32_blackpill.render(5).color((0.2, 0.2, 0.2))
-        + up(20)(
+        + (
             stm32_blackpill.render(5).color((0.2, 0.2, 0.2))
             + stm32_blackpill.board_profile(5).color((0.0, 0.4, 0.0))
-        )
+        ).up(20)
     ).save_as_scad()
